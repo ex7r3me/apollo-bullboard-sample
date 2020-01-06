558d3851dd3028ba1e97a19a5d80220a893d701b
ex7r3me <siavash.ahmadpour@gmail.com>
2020-01-06 22:52:25 +0300

    Add bull-board
    - test bull-board with apollo server

--- server.js
+++ server.js
@@ -2,14 +2,29 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 import cors from 'cors';
+import { UI } from 'bull-board'
+const { createQueues } = require('bull-board')
 
 import schema from './data/schema';
-
 const PORT = 8080;
+const redisConfig = {
+  redis: {
+    port: 6379,
+    host: 'localhost',
+  },
+}
 
+const queues = createQueues(redisConfig)
+const helloQueue = queues.add('helloQueue') 
+helloQueue.process(async job => {
+  console.log(`Hello ${job.data.hello}`)
+})
+
+helloQueue.add({ hello: 'world' }) 
 const app = express();
 
 app.use('*', cors());
+app.use('/admin/queue', UI)
 
 const server = new ApolloServer({ 
   schema, 
